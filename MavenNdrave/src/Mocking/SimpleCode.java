11075df1ccdfa97dff7a9243800ab7e0a8f3a0ec
shubhamkcse <shubhamkcse@gmail.com>
2022-03-04 06:10:22 -0800

    done

---
+++ MavenNdrave/src/Mocking/SimpleCode.java
@@ -0,0 +1,19 @@
+package Mocking;
+
+import static org.mockito.Mockito.mock;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class SimpleCode {
+	
+	@Test
+	public void testSimpleCode()
+	{
+		List mc=Mockito.mock(List.class);
+	}
+	
+}
