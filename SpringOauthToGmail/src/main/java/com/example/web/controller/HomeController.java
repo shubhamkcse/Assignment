c08c75a1b46db7939297bcda59702c497bf7ad9b
shubhamkcse <shubhamkcse@gmail.com>
2022-06-05 21:50:25 -0700

    Done

---
+++ SpringOauthToGmail/src/main/java/com/example/web/controller/HomeController.java
@@ -0,0 +1,22 @@
+package com.example.web.controller;
+
+import java.security.Principal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String home()
+	{
+		return "any user can access this!!";
+	}
+	
+	@GetMapping("/prevent")
+	public Principal prevent(Principal principal)
+	{
+		return principal;
+	}
+}
---
+++ SpringOauthToGmail/src/test/java/com/example/web/SpringOauthToGmailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringOauthToGmailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
