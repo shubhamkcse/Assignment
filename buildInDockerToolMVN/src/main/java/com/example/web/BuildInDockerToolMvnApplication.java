4594dcb2e264db26d3dd6ada6364df24050720de
shubhamkcse <shubhamkcse@gmail.com>
2022-06-05 21:46:44 -0700

    Done

---
+++ buildInDockerToolMVN/src/main/java/com/example/web/BuildInDockerToolMvnApplication.java
@@ -0,0 +1,12 @@
+package com.example.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildInDockerToolMvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildInDockerToolMvnApplication.class, args);
+	}
+}
